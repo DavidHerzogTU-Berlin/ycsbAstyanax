2135a1c1cd478106522bee6766773463e0c359cc
David <david.herzog@campus.tu-berlin.de>
2014-09-01 15:34:05 +0200

    added astyanax Build

---
+++ astyanaxToBuild/beforeGradle.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "delete jcraft"
+rm -r ~/.m2/repository/com/jcraft/
+./gradlew build -x test
+echo "copy astyanax-core to maven repo. and replace old version"
+cp astyanax-core/build/libs/astyanax-core-2.0.2-SNAPSHOT.jar ~/.m2/repository/com/netflix/astyanax/astyanax-core/1.56.42
+rm ~/.m2/repository/com/netflix/astyanax/astyanax-core/1.56.42/astyanax-core-1.56.42.jar
+mv ~/.m2/repository/com/netflix/astyanax/astyanax-core/1.56.42/astyanax-core-2.0.2-SNAPSHOT.jar ~/.m2/repository/com/netflix/astyanax/astyanax-core/1.56.42/astyanax-core-1.56.42.jar 
+echo "copy astyanax-thrift to maven repo. and replace old version"
+cp astyanax-thrift/build/libs/astyanax-thrift-2.0.2-SNAPSHOT.jar ~/.m2/repository/com/netflix/astyanax/astyanax-thrift/1.56.42
+rm ~/.m2/repository/com/netflix/astyanax/astyanax-thrift/1.56.42/astyanax-thrift-1.56.42.jar
+mv ~/.m2/repository/com/netflix/astyanax/astyanax-thrift/1.56.42/astyanax-thrift-2.0.2-SNAPSHOT.jar ~/.m2/repository/com/netflix/astyanax/astyanax-thrift/1.56.42/astyanax-thrift-1.56.42.jar 
