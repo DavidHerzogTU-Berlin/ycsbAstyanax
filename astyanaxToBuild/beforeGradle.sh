88ee462ebda112dce63cd69f2f70c25eeaf46f62
David <david.herzog@campus.tu-berlin.de>
2014-09-11 18:12:42 +0200

    now replacing thrift, core, cassandra

--- astyanaxToBuild/beforeGradle.sh
+++ astyanaxToBuild/beforeGradle.sh
@@ -6,7 +6,13 @@ echo "copy astyanax-core to maven repo. and replace old version"
 cp astyanax-core/build/libs/astyanax-core-2.0.2-SNAPSHOT.jar ~/.m2/repository/com/netflix/astyanax/astyanax-core/1.56.42
 rm ~/.m2/repository/com/netflix/astyanax/astyanax-core/1.56.42/astyanax-core-1.56.42.jar
 mv ~/.m2/repository/com/netflix/astyanax/astyanax-core/1.56.42/astyanax-core-2.0.2-SNAPSHOT.jar ~/.m2/repository/com/netflix/astyanax/astyanax-core/1.56.42/astyanax-core-1.56.42.jar 
+
 echo "copy astyanax-thrift to maven repo. and replace old version"
 cp astyanax-thrift/build/libs/astyanax-thrift-2.0.2-SNAPSHOT.jar ~/.m2/repository/com/netflix/astyanax/astyanax-thrift/1.56.42
 rm ~/.m2/repository/com/netflix/astyanax/astyanax-thrift/1.56.42/astyanax-thrift-1.56.42.jar
-mv ~/.m2/repository/com/netflix/astyanax/astyanax-thrift/1.56.42/astyanax-thrift-2.0.2-SNAPSHOT.jar ~/.m2/repository/com/netflix/astyanax/astyanax-thrift/1.56.42/astyanax-thrift-1.56.42.jar 
+mv ~/.m2/repository/com/netflix/astyanax/astyanax-thrift/1.56.42/astyanax-thrift-2.0.2-SNAPSHOT.jar ~/.m2/repository/com/netflix/astyanax/astyanax-thrift/1.56.42/astyanax-thrift-1.56.42.jar
+
+echo "copy astyanax-cassandra to maven repo. and replace old version"
+cp astyanax-cassandra/build/libs/astyanax-cassandra-2.0.2-SNAPSHOT.jar ~/.m2/repository/com/netflix/astyanax/astyanax-cassandra/1.56.42
+rm ~/.m2/repository/com/netflix/astyanax/astyanax-cassandra/1.56.42/astyanax-cassandra-1.56.42.jar
+mv ~/.m2/repository/com/netflix/astyanax/astyanax-cassandra/1.56.42/astyanax-cassandra-2.0.2-SNAPSHOT.jar ~/.m2/repository/com/netflix/astyanax/astyanax-cassandra/1.56.42/astyanax-cassandra-1.56.42.jar 
